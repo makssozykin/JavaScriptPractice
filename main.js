cfc32e5acab00e78d7cc5a9c97c32717b5925ed0
makssozykin <mvsozykin@gmail.com>
2023-06-11 17:39:46 +0300

    JavaScriptPractice

--- main.js
+++ main.js
@@ -41,14 +41,64 @@
 // makeTransaction(1000, 3, 15000);
 // makeTransaction(500, 10, 5000);
 
-const courseTopic = "JavaScript essentials";
-// Change code below this line
+// const courseTopic = "JavaScript essentials";
+// // Change code below this line
 
-const courseTopicLength = courseTopic.length;
-const firstElement = courseTopic[0];
-const lastElement = courseTopic[20];
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[20];
 
-// Change code above this line
-console.log(courseTopicLength);
-console.log(firstElement);
-console.log(lastElement);
+// // Change code above this line
+// console.log(courseTopicLength);
+// console.log(firstElement);
+// console.log(lastElement);
+
+// const clients = ["Mango", "Ajax", "Poly"];
+
+// for (let i = 0; i < clients.length; i += 1) {
+//   console.log(clients[i]);
+// }
+
+// const clients = ["Mango", "Ajax", "Poly"];
+
+// for (const client of clients) {
+//   console.log(client);
+// }
+
+// const string = "javascript";
+
+// for (const character of string) {
+//   console.log(character);
+// }
+
+// const clients = ["Mango", "Poly", "Ajax"];
+// const clientNameToFind = "Poly";
+// let message;
+
+// for (const client of clients) {
+//   // На кожній ітерації будемо перевіряти чи збігається елемент масиву з
+//   // іменем клієнта. Якщо збігається - записуємо в message повідомлення
+//   // про успіх і робимо break, щоб далі не шукати
+//   if (client === clientNameToFind) {
+//     message = "Клієнт з таким ім'ям є в базі даних!";
+//     break;
+//   }
+
+//   // Якщо вони не збігаються - записуємо в message повідомлення про відсутність імені
+//   message = "Клієнт з таким ім'ям відсутній в базі даних!";
+// }
+
+// console.log(message); // "Клієнт з таким ім'ям є в базі даних!"
+
+const numbers = [1, 3, 14, 18, 4, 7, 29, 6, 34];
+const threshold = 15;
+
+// Для чисел, менших ніж порогове значення, спрацьовує continue, виконання тіла
+// припиняється і управління передається на наступну ітерацію.
+for (let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] > threshold) {
+    continue;
+  }
+
+  console.log(`Число більше за ${threshold}: ${numbers[i]}`); // 18, 29, 34
+}
